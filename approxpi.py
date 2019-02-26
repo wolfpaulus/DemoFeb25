3ed6166b868b4b41870557a01b2b304c450f77ec
Wolf Paulus <wolf@wolfpaulus.com>
2019-02-25 19:48:08 -0800

    1st commit

---
+++ approxpi.py
@@ -0,0 +1,20 @@
+"""
+ I'd like to approximate pi using the monte carlo method and python's random number generator.
+"""
+import math
+import random
+
+
+k = 0
+n = 0
+approx = 0;
+while abs(approx - math.pi) < ...:  # todo until the approx is good enough.
+    n += 1
+    x, y = random.random(), random.random()
+    # is the coordinate (x,y) inside the circle or not?
+
+    if (x * x + y * y) <= 1:  # yes it's inside.
+        k += 1
+
+    approx = 4 * k / n
+print("..." + str(n))
